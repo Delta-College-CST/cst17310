b35256077b89b75832134a04f9ec2546e2c81115
teklingl <teklingl@gmail.com>
2024-09-02 09:51:08 -0400

    Examples/Practice

---
+++ listops.py
@@ -0,0 +1,15 @@
+# This program demonstrates various basic Python list actions
+
+infolist = [ 4, 3, 5, 6, 2, 8, 1 ]  # Declare an initialize a list
+
+print(infolist)                     # Print list contents
+
+print(infolist[0])                  # Access list elements - first
+print(infolist[3])                  # Access list elements - 4th
+print(infolist[6])                  # Access list elements - last
+
+listLength = len(infolist)          # Access
+print(listLength)                   # and print list length      
+
+for listItem in infolist:           # Iterate through list and
+    print(listItem,end=' ')         # print on same line
